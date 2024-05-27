d33b23afb128546ff776ed17ff349f465a958201
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2024-05-27 09:26:22 -0300

    menu component

---
+++ src/components/Menu.tsx
@@ -0,0 +1,21 @@
+import { FC, memo } from "react";
+import MenuItem from "./MenuItem";
+
+type MenuProps = {};
+
+const Menu: FC<MenuProps> = () => {
+  return (
+    <nav className="mt-12">
+      <ul className="w-100 text-gray-50 flex justify-center gap-4">
+        <MenuItem href="/">Home</MenuItem>
+        <MenuItem href="#about">About</MenuItem>
+        <MenuItem href="#projects">Projects</MenuItem>
+        <MenuItem href="https://github.com/pedrodnbr" target="_blank">
+          GitHub
+        </MenuItem>
+      </ul>
+    </nav>
+  );
+};
+
+export default memo(Menu);
