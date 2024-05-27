d3431c3aaf37ddea66b348dfea1e06fbb68c999f
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2024-05-27 10:47:15 -0300

    about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
+import About from "@/components/About";
 import Header from "@/components/Header";
 import Menu from "@/components/Menu";
-import { Fragment } from "react";
 
 export default function Home() {
   return (
     <div className="w-[1000px]">
       <Menu />
       <Header />
+
+      <main className="flex flex-col gap-16">
+        <About />
+      </main>
     </div>
   );
 }
---
+++ src/components/About.tsx
@@ -0,0 +1,29 @@
+import { FC, memo } from "react";
+
+type AboutProps = {};
+
+const About: FC<AboutProps> = () => {
+  return (
+    <section className="flex flex-col gap-8">
+      <h2 className="section-heading">About me</h2>
+      <div className="flex flex-col gap-6">
+        <p className="paragraph">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis
+          lacus ultrices, fringilla dolor nec, hendrerit mi. Etiam mauris eros,
+          <strong>scelerisque nec facilisis</strong> ut, vestibulum sit amet
+          dui. Integer magna elit, eleifend bibendum tincidunt in, tristique
+          eget quam. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Fusce quis lacus ultrices, fringilla dolor nec, hendrerit mi.
+        </p>
+        <p className="paragraph">
+          Maecenas iaculis tempor tortor a venenatis. Etiam{" "}
+          <strong>gravida</strong> mi porttitor diam ultrices malesuada.
+          Phasellus convallis, dolor eget venenatis cursus, turpis erat auctor
+          metus, at tincidunt
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default memo(About);
