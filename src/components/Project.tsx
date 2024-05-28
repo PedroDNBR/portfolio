4f93015d7b6b572481434d7b9e674eece4895cc4
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2024-05-28 10:59:41 -0300

    hover effect on project item

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,5 +1,6 @@
 import Image, { StaticImageData } from "next/image";
 import { FC, memo } from "react";
+import { RiCursorLine } from "react-icons/ri";
 
 type ProjectProps = {
   title: string;
@@ -10,7 +11,13 @@ type ProjectProps = {
 const Project: FC<ProjectProps> = ({ title, excerpt, image }) => {
   return (
     <div>
-      <Image className="rounded mb-4" src={image} alt={title} />
+      <div className="group mb-4 relative overflow-hidden rounded">
+        <span className="absolute font-medium w-full h-full flex justify-center items-center gap-2 bg-black/75 opacity-0 hover:opacity-100 transition cursor-pointer">
+          <RiCursorLine size={24} />
+          Click to show more
+        </span>
+        <Image src={image} alt={title} />
+      </div>
       <div className="flex flex-col gap-2">
         <span className="font-bold text-lg">{title}</span>
         <p className="paragraph">{excerpt}</p>
